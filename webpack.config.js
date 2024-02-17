8fbca8219a5e8493ef4a272b307842d26bbca972
Matt Lammon <mlammon11@gmail.com>
2024-02-16 20:58:12 -0500

    Webpack attempt
    Attempted using webpack to combine 4 javascript files into one called 'bundle.js'

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,32 @@
+// webpack.config.js
 const path = require('path');
+
+module.exports = {
+    context: __dirname,
+    entry: { 
+        main: [
+            './app/js/webpack/leaflet.js',      //sets entry points for webpack to combine these js files into one
+            './app/js/webpack/turf.js',
+            './app/js/main.js',
+            './app/js/vendor/unmute.js'
+        ]
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),  //outputs the file 'bundle.js' to the folder 'dist'
+        filename: 'bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    }
+};
