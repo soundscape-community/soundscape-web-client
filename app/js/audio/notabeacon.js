ee04a299aae12c5c0afe617dd599696d1556c422
Daniel W. Steinbrook <dsteinbrook@gmail.com>
2024-03-17 11:12:26 -0400

    Move beacon update logic out of audio event handler
    Possible fix for #8

--- app/js/audio/notabeacon.js
+++ app/js/audio/notabeacon.js
@@ -34,46 +34,56 @@ export function createBeacon(latitude, longitude, locationProvider, audioQueue) 
       onCourse.play();
       offCourse.play();
     },
+
     stop: () => {
       onCourse.pause();
       offCourse.pause();
     },
-    recomputePosition: () => {
-      relativePosition = locationProvider.normalizedRelativePosition(sourceLocation);
-      panner.setPosition(relativePosition.x, relativePosition.y, 0);
-    },
-  };
 
-  // Reevaluate beacon state at most once per audio loop
-  // "ended" event is not triggered by looped audio, so approximate restart based on time
-  onCourse.addEventListener('timeupdate', () => {
-    if (onCourse.currentTime < 0.1) {
-      const distanceMeters = locationProvider.distance(sourceLocation, { units: "meters" });
-      if (distanceMeters < foundProximityMeters) {
-        // Beacon found -- stop the audio
-        beacon.stop();
-        new Audio('app/sounds/SS_beaconFound2_48k.wav').play();
-      } else if (Math.abs(lastAnnouncedDistance - distanceMeters) > announceEveryMetters) {
-        // We're closer/further by some threshold -- announce distance
-        // Only announce if not actively playing something else (distance would be stale if queued)
-        if (!audioQueue.isPlaying) {
-          lastAnnouncedDistance = distanceMeters;
-          audioQueue.addToQueue({ soundUrl: 'app/sounds/sense_mobility.wav' })
-          audioQueue.addToQueue({ text: `Beacon: ${distanceMeters.toFixed(0)} meters` })
-        }
+    iEnabled: () => (!onCourse.paused || !offCourse.paused),
+
+    announceDistance: (distanceMeters) => {
+      // Only announce if not actively playing something else (distance would be stale if queued)
+      if (!audioQueue.isPlaying) {
+        lastAnnouncedDistance = distanceMeters;
+        audioQueue.addToQueue({ soundUrl: 'app/sounds/sense_mobility.wav' })
+        audioQueue.addToQueue({ text: `Beacon: ${distanceMeters.toFixed(0)} meters` })
+      }
+    },
+
+    setOnOffCourse: (relativePosition) => {
+      // Transition between "on" and "off" beacons
+      const angle = Math.atan2(relativePosition.x, relativePosition.y) * 180 / Math.PI;
+      if (Math.abs(angle) < onCourseAngle) {
+        onCourse.muted = false;
+        offCourse.muted = true;
       } else {
-        // Transition between "on" and "off" beacons
-        const angle = Math.atan2(relativePosition.x, relativePosition.y) * 180 / Math.PI;
-        if (Math.abs(angle) < onCourseAngle) {
-          onCourse.muted = false;
-          offCourse.muted = true;
-        } else {
-          onCourse.muted = true;
-          offCourse.muted = false;
+        onCourse.muted = true;
+        offCourse.muted = false;
+      }
+    },
+
+    recomputePosition: () => {
+      // Reevaluate how on-course we are
+      if (beacon.iEnabled()) {
+        relativePosition = locationProvider.normalizedRelativePosition(sourceLocation);
+        panner.setPosition(relativePosition.x, relativePosition.y, 0);
+
+        const distanceMeters = locationProvider.distance(sourceLocation, { units: "meters" });
+        if (distanceMeters < foundProximityMeters) {
+          // Beacon found -- stop the audio
+          beacon.stop();
+          new Audio('app/sounds/SS_beaconFound2_48k.wav').play();
+        } else if (Math.abs(lastAnnouncedDistance - distanceMeters) > announceEveryMetters) {
+          // We're closer/further by some threshold -- announce distance
+          beacon.announceDistance(distanceMeters);
+        } else if (onCourse.currentTime < 0.1) {
+          // Update the beacon sound, if just looped
+          beacon.setOnOffCourse(relativePosition);
         }
       }
-    }
-  });
+    },
+  };
 
   // Hook up listeners
   locationProvider.events.addEventListener('locationUpdated', beacon.recomputePosition)
