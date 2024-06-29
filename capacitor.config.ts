eef53fe81666497cf099ec4da8dc09938c912dfb
Sebastian Ovide <sebastian.ovide@gmail.com>
2024-06-29 11:47:51 +0100

    finished the merging. Publishing the dist folder to github pages

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'MyFirstIonic',
+  appId: 'services.soundscape.multiplatform',
+  appName: 'SoundscapeMultiplatform',
   webDir: 'dist'
 };
 
