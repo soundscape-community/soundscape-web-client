0d498cfa30fd871d961b088e79608bb8f47808fe
Sebastian Ovide <sebastian.ovide@gmail.com>
2024-06-30 20:39:53 +0100

    text to speach via capacitor plugin

--- src/js/visual/voicecontrols.js
+++ src/js/visual/voicecontrols.js
@@ -1,37 +1,67 @@
 // Copyright (c) Daniel W. Steinbrook.
 // with many thanks to ChatGPT
+import { TextToSpeech } from "@capacitor-community/text-to-speech";
 
 function createVoiceControls(audioQueue) {
   // Fetch available voices
-  const voiceSelect = document.getElementById('voice');
+  const voiceSelect = document.getElementById("voice");
   // const rateInput = document.getElementById('rate');
-  const decreaseRate = document.getElementById('decreaseRate');
-  const increaseRate = document.getElementById('increaseRate');
-  const rateValue = document.getElementById('rateValue');
+  const decreaseRate = document.getElementById("decreaseRate");
+  const increaseRate = document.getElementById("increaseRate");
+  const rateValue = document.getElementById("rateValue");
+
+  // Just for testing
+  // TextToSpeech.speak({
+  //   text: "This is a sample text.",
+  //   lang: "en-US",
+  //   rate: 1.0,
+  //   pitch: 1.0,
+  //   volume: 1.0,
+  //   category: "ambient",
+  // });
 
   // Populate voice selector
   function populateVoices() {
     // Populate voice list with all English voices
-    audioQueue.voices = window.speechSynthesis.getVoices()
-      .filter(voice => voice.lang.startsWith('en'));;
-    audioQueue.voices.forEach(function(voice, index) {
-      const option = document.createElement('option');
-      option.value = index;
-      option.textContent = '🗣 ' + voice.name;
-      voiceSelect.appendChild(option);
+    audioQueue.voices = [];
+
+    TextToSpeech.getSupportedVoices().then((voices) => {
+      const voicesEn = voices.voices.filter((voice) =>
+        voice.lang.startsWith("en")
+      );
+      const voicesNames = new Set(voicesEn.map((voice) => voice.name));
+
+      audioQueue.voices = Array.from(voicesNames).map((name) =>
+        voicesEn.find((voice) => voice.name === name)
+      );
+
+      // Remove them to avoid duplicates
+      while (voiceSelect.childNodes[0] != null) {
+        voiceSelect.childNodes[0].remove();
+      }
+
+      // console.log(`I'll add ${audioQueue.voices.length} voices to the list`);
+      audioQueue.voices.forEach(function (voice, index) {
+        const option = document.createElement("option");
+        option.value = index;
+        option.textContent = "🗣 " + voice.name;
+        voiceSelect.appendChild(option);
+
+        console.log(`VOICE ${voice.name} with value ${index} APPENDED`);
+      });
     });
   }
   populateVoices();
 
   // Select the system default voice by default
-  const systemDefaultVoice = audioQueue.voices.find(voice => voice.default);
+  const systemDefaultVoice = audioQueue.voices.find((voice) => voice.default);
   if (systemDefaultVoice) {
     voiceSelect.value = audioQueue.voices.indexOf(systemDefaultVoice);
   }
 
   // Update voices when they change
-  window.speechSynthesis.onvoiceschanged = function() {
-    voiceSelect.innerHTML = ''; // Clear existing options
+  window.speechSynthesis.onvoiceschanged = function () {
+    voiceSelect.innerHTML = ""; // Clear existing options
     populateVoices();
   };
 
@@ -44,7 +74,7 @@ function createVoiceControls(audioQueue) {
     rateValue.textContent = audioQueue.increaseRate();
   });
 
-  voiceSelect.addEventListener('change', function() {
+  voiceSelect.addEventListener("change", function () {
     audioQueue.setVoice(voiceSelect.value);
   });
 
