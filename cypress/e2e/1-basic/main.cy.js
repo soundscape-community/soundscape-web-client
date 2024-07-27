7e93a9a2c726f505c60f7bf47b0666636a271065
Daniel W. Steinbrook <dsteinbrook@gmail.com>
2024-07-27 07:59:23 -0400

    Add GPX map initialization test (not passing in headless)

--- cypress/e2e/1-basic/main.cy.js
+++ cypress/e2e/1-basic/main.cy.js
@@ -9,4 +9,24 @@ describe('GPX view', () => {
     cy.get('button').should('have.length', 1)
     cy.get('button').first().should('have.text', 'Start exploring')
   })
+
+  it('sets map to first point in the GPX file', () => {
+    cy.get('button').click()
+    cy.get('#gpxFileInput').selectFile('cypress/fixtures/waterloo.gpx')
+
+    const expectedLat = 43.464;
+    const expectedLng = -80.525;
+    const expectedZoom = 16;
+
+    cy.get('#map').should(($map) => {
+      const map = $map[0]._leafletMap;
+
+      const center = map.getCenter();
+      expect(center.lat).to.be.closeTo(expectedLat, 0.001);
+      expect(center.lng).to.be.closeTo(expectedLng, 0.001);
+
+      const zoom = map.getZoom();
+      expect(zoom).to.equal(expectedZoom);
+    });
+  })
 })
