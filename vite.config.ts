47202e4089d1ff75cf9eb62fe9e1f2c55af983d6
Daniel W. Steinbrook <dsteinbrook@gmail.com>
2024-11-17 20:22:24 -0500

    WIP: TypeScript

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { VitePWA } from 'vite-plugin-pwa';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   plugins: [
     vue(),
+    tsconfigPaths(),
     VitePWA({
       registerType: 'prompt',
       injectRegister: false,
