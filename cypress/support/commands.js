611b23fcd7b852f85542b70a6c4a9bcc4abdb9d2
Daniel W. Steinbrook <dsteinbrook@gmail.com>
2024-11-19 20:06:45 -0500

    Add beacon.cy.js test

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -5,6 +5,25 @@ Cypress.Commands.add('mockSpeechSynthesis', () => {
     cy.stub(win.speechSynthesis, 'cancel').as('cancel');
     cy.stub(win.speechSynthesis, 'pause').as('pause');
     cy.stub(win.speechSynthesis, 'resume').as('resume');
-    cy.stub(win.speechSynthesis, 'getVoices').returns([]).as('getVoices');
+    cy.stub(win.speechSynthesis, 'getVoices').returns([{
+      "name": "John Doe",
+      "lang": "en-US"
+    }]).as('getVoices');
+  });
+});
+
+// Mock geolocation API for headless testing
+Cypress.Commands.add('mockGeolocation', (latitude, longitude) => {
+  cy.window().then((win) => {
+    cy.stub(win.navigator.geolocation, 'getCurrentPosition').callsFake((cb) => {
+      return cb({ coords: { latitude, longitude } });
+    });
+  });
+});
+
+// Check if audio is playing for beacon testing
+Cypress.Commands.add('isAudioPlaying', () => {
+  cy.window().then((win) => {
+    return (win.navigator.mediaSession.playbackState == 'playing');
   });
 });
