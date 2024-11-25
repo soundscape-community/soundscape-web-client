8297fc87b8b3c766ede798d129dcfb7abb04b060
Daniel W. Steinbrook <dsteinbrook@gmail.com>
2024-11-24 20:37:16 -0500

    Unit tests for enumerateTilesAround

--- src/composables/tile.test.ts
+++ src/composables/tile.test.ts
@@ -2,6 +2,7 @@ import { expect } from "chai";
 import {
   createBoundingBox,
   latLonToTileCoords,
+  enumerateTilesAround,
 } from "./tile";
 
 describe("createBoundingBox", () => {
@@ -38,3 +39,23 @@ describe("latLonToTileCoords", () => {
     expect(result).to.deep.equal({ x: 6, y: 4, z: 3 });
   });
 });
+
+describe("enumerateTilesAround", () => {
+  it('should return tiles around location', () => {
+    // 10m radius around Washington Monument
+    const result = enumerateTilesAround(38.889444, -77.035278, 10)
+    expect(result.length).to.equal(1);
+    expect(result[0]).to.include({
+      x: 18744,
+      y: 25072,
+      z: 16,
+      key: '16/18744/25072',
+    });
+  });
+
+  it('should return more tiles for a bigger radius', () => {
+    // 1km radius around Washington Monument
+    const result = enumerateTilesAround(38.889444, -77.035278, 1000)
+    expect(result.length).to.equal(25);
+  });
+})
