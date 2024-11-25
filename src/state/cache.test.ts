514db99513d5ddb3992172d83ffed86fdf73f993
Daniel W. Steinbrook <dsteinbrook@gmail.com>
2024-11-25 08:43:19 -0500

    Use fake-indexeddb to fix cache unit tests

--- src/state/cache.test.ts
+++ src/state/cache.test.ts
@@ -1,38 +1,49 @@
-import { cache } from "./cache";
+// Mock out IndexedDB for testing
+import 'fake-indexeddb/auto';
+
+import { cache, SoundscapeFeature } from "./cache";
 import { expect } from "chai";
 
+// Real feature extracted from tile
+const testFeature: SoundscapeFeature = {
+  "feature_type": "amenity",
+  "feature_value": "cafe",
+  "geometry": {
+    "coordinates": [-80.52166, 43.463198],
+    "type": "Point"
+  },
+  "osm_ids": [503048818],
+  "properties": {
+    "amenity": "cafe",
+    "cuisine": "bubble_tea",
+    "name": "The Bingsu"
+  },
+  "type": "Feature"
+};
+
 describe("cache", () => {
-  it('should be empty for unloaded tiles', () => {
-    cache.getFeatures("16/18109/23965").then((result) => {
-      expect(result.length).to.equal(0);
-    });
+  it('should be empty for unloaded tiles', async () => {
+    const result = await cache.getFeatures("16/18109/23965");
+    expect(result.length).to.equal(0);
   });
 
-  it('should store and retrieve features', () => {
-    // Real feature extracted from tile
-    cache.addFeature({
-      "feature_type": "amenity",
-      "feature_value": "cafe",
-      "geometry": {
-        "coordinates": [-80.52166, 43.463198],
-        "type": "Point"
-      },
-      "osm_ids": [503048818],
-      "properties": {
-        "amenity": "cafe",
-        "cuisine": "bubble_tea",
-        "name": "The Bingsu"
-      },
-      "type": "Feature"
-    },
-    "16/18109/23965");
+  it('should store and retrieve features', async () => {
+    cache.addFeature(testFeature, "16/18109/23965");
+    const result = await cache.getFeatures("16/18109/23965");
+    expect(result.length).to.equal(1);
+    // Original feature will be annotated with an id
+    expect(result[0]).to.deep.equal({...testFeature, id: 1});
+  });
 
-    cache.getFeatures("16/18109/23965").then((result) => {
-      expect(result.length).to.equal(1);
-      expect(result[0]).to.include({
-        "feature_type": "amenity",
-        "feature_value": "cafe",  
-      });
-    });
+  it ('should fetch features by OSM ID', async () => {
+    cache.addFeature(testFeature, "16/18109/23965");
+    const result = await cache.getFeatureByOsmId(testFeature.osm_ids[0]);
+    // Original feature will be annotated with an id
+    expect(result).to.deep.equal({...testFeature, id: 1});
+  });
+
+  it ('should return no results for invalid OSM ID', async () => {
+    const result = await cache.getFeatureByOsmId(9999999);
+    expect(result).to.equal(null);
   });
 });
